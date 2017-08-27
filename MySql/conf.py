83fa23caf32bb01abcc6fdacae7c1167f5863810
MrAlex6204 <engineer.vera@yahoo.com>
2017-08-27 16:12:01 -0500

    Python and MySql
    In these exercises it's show how to connect to Mysql database
    and how to retrieve data from a Web Service and inserted into a
    table in MySql

---
+++ MySql/conf.py
@@ -0,0 +1,8 @@
+#Add edit here your db connection setting to MySql
+#Database Authentication Credentilas
+auth = {
+    "host":"bookstorage.online",
+    "user":"admin",
+    "password":"lealtad6204....",
+    "database":"dbTest"
+}
